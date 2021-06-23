c043b403bfadf51ff799e3140cd6e79dc644af84
Felix <felix.wensky@gmail.com>
2021-06-23 21:59:11 +0200

    Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MemoryGUI.fxml"));
+
+        primaryStage.setTitle("Knerten Memory");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
