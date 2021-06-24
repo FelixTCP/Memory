3cc22924f16d3e785d50a2b1c0a25c08c4ef0ae0
Felix <felix.wensky@gmail.com>
2021-06-24 21:11:48 +0200

    Added Attribute imgURL Added Attribute pairID Added Constructor Added Getter

--- src/application/logic/Card.java
+++ src/application/logic/Card.java
@@ -1,4 +1,19 @@
 package application.logic;
 
 public class Card {
+    private String imgURL;
+    private int pairID;
+
+    public Card(String imgURL, int pairID) {
+        this.imgURL = imgURL;
+        this.pairID = pairID;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public int getPairID() {
+        return pairID;
+    }
 }
